eec5f4316c6bcdd1b06045f42ff288a3f1c933b3
ethas <liebeedelvais@gmail.com>
2025-06-12 16:56:04 +0700

    git: add controller

---
+++ TutorBooking.APIService/Controllers/ScheduleController.cs
@@ -0,0 +1,35 @@
+﻿using App.Core.Base;
+using App.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TutorBooking.APIService.Controllers
+{
+    [Route("api/schedule")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ScheduleController : ControllerBase
+    {
+        #region DI Constructor
+        private readonly IScheduleService _scheduleService;
+
+        public ScheduleController(IScheduleService scheduleService)
+        {
+            _scheduleService = scheduleService;
+        }
+        #endregion
+
+        [HttpGet("tutors/{tutorId}/availability")]
+        public async Task<IActionResult> GetTutorAvailability(
+            [FromRoute] string tutorId,
+            [FromQuery] DateTime startDate,
+            [FromQuery] DateTime endDate)
+        {
+            var availability = await _scheduleService.GetTutorAvailabilityAsync(tutorId, startDate, endDate);
+            return Ok(new BaseResponseModel<List<DailyAvailabilityDTO>>(
+                data: availability,
+                message: "Lịch của gia sư"
+            ));
+        }
+    }
+}
