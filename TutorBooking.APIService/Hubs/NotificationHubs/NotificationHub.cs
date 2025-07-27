62ca8b3640f9328319b6d1abe43fb3e1683e6719
bacsiemon <johnsieunhan816@gmail.com>
2025-07-27 11:44:58 +0700

    [minor] Changes to Exception Messages

--- TutorBooking.APIService/Hubs/NotificationHubs/NotificationHub.cs
+++ TutorBooking.APIService/Hubs/NotificationHubs/NotificationHub.cs
@@ -1,10 +1,12 @@
 ﻿using App.Core.Base;
+using App.Core.Constants;
 using App.DTOs.NotificationDTOs;
 using App.Repositories.Models.User;
 using App.Services.Interfaces;
 using App.Services.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Net.Http.Headers;
 using Org.BouncyCastle.Asn1.Ocsp;
 using System.IdentityModel.Tokens.Jwt;
@@ -29,21 +31,20 @@ namespace TutorBooking.APIService.Hubs.NotificationHubs
         }
 
         public override async Task OnConnectedAsync()
-		{
-			await base.OnConnectedAsync();
-            var userId = GetUserId();
-            var roles = GetRole();
-            _userIdMapper.Remove(userId);
-            _userIdMapper.TryAdd(userId, Context.ConnectionId);
-            foreach (var role in GetRole())
-                await Groups.AddToGroupAsync(Context.ConnectionId, role.ToStringRole());
-			await Clients.Client(Context.ConnectionId).UserConnected("CONNECTED_TO_NOTIFICATION_HUB");
+        {
+                await base.OnConnectedAsync();
+                var userId = GetUserId();
+                var roles = GetRole();
+                _userIdMapper.Remove(userId);
+                _userIdMapper.TryAdd(userId, Context.ConnectionId);
+                foreach (var role in GetRole())
+                    await Groups.AddToGroupAsync(Context.ConnectionId, role.ToStringRole());
+                await Clients.Client(Context.ConnectionId).UserConnected("CONNECTED_TO_NOTIFICATION_HUB");
 		}
 
 		public override async Task OnDisconnectedAsync(Exception? exception)
 		{
 			await base.OnDisconnectedAsync(exception);
-			await Clients.All.UserDisconnected(Context.ConnectionId);
 		}
 
         public async Task MarkAsRead(string notificationId)
@@ -58,7 +59,7 @@ namespace TutorBooking.APIService.Hubs.NotificationHubs
                 _logger.LogError(ex.ToString());
                 var connectionId = _userIdMapper.GetValueOrDefault(GetUserId());
                 if (connectionId != null)
-                    await Clients.Client(connectionId).MarkAsReadResult(ex.StatusCode, ex.ErrorDetail);
+                    await Clients.Client(connectionId).MarkAsReadResult(ex.StatusCode, ex.Message);
             }
             catch (Exception ex)
             {
@@ -72,20 +73,34 @@ namespace TutorBooking.APIService.Hubs.NotificationHubs
 
         private string GetUserId()
         {
-            var token = Context.GetHttpContext().Request.Headers[HeaderNames.Authorization].ToString().Substring("Bearer ".Length).Trim();
-            var handler = new JwtSecurityTokenHandler();
-            var securityToken = handler.ReadJwtToken(token);
-            var userId = securityToken.Claims.FirstOrDefault(c => c.Type.Equals(JwtRegisteredClaimNames.Sub)).Value;
-            return userId;
+            try
+            {
+                var token = Context.GetHttpContext().Request.Headers[HeaderNames.Authorization].ToString().Substring("Bearer ".Length).Trim();
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.ReadJwtToken(token);
+                var userId = securityToken.Claims.FirstOrDefault(c => c.Type.Equals(JwtRegisteredClaimNames.Sub)).Value;
+                return userId;
+            } 
+            catch (Exception ex)
+            {
+                throw new Exception("FAILED_TO_GET_USER_ID_FROM_TOKEN");
+            }      
         }
 
         private List<Role> GetRole()
         {
-            var token = Context.GetHttpContext().Request.Headers[HeaderNames.Authorization].ToString().Substring("Bearer ".Length).Trim();
-            var handler = new JwtSecurityTokenHandler();
-            var securityToken = handler.ReadJwtToken(token);
-            var roles = securityToken.Claims.Where(c => c.Type.Equals(ClaimTypes.Role)).Select(x => x.Value.ToRoleEnum()).ToList();
-            return roles;
+            try
+            {
+                var token = Context.GetHttpContext().Request.Headers[HeaderNames.Authorization].ToString().Substring("Bearer ".Length).Trim();
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.ReadJwtToken(token);
+                var roles = securityToken.Claims.Where(c => c.Type.Equals(ClaimTypes.Role)).Select(x => x.Value.ToRoleEnum()).ToList();
+                return roles;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception( "FAILED_TO_GET_ROLE_FROM_TOKEN");
+            }
         } 
     }
 
