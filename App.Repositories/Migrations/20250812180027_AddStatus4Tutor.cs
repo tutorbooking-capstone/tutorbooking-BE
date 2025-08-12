412b6c80dae2d3e8b70a06a8e37080159d5a3027
ethas <liebeedelvais@gmail.com>
2025-08-13 01:07:59 +0700

    chore: add migrate 4 status in tutor

---
+++ App.Repositories/Migrations/20250812180027_AddStatus4Tutor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatus4Tutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "verification_status",
+                table: "tutors",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "verification_status",
+                table: "tutors");
+        }
+    }
+}
